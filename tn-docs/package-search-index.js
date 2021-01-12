f6f4fd80fe6cf2fa9a2976f5db9199569dde4320
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 13:48:28 -0500

    Add javadocs

---
+++ tn-docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.tweetninja.cli"},{"l":"io.tweetninja.cli.Asides"},{"l":"io.tweetninja.cli.Datatypes"},{"l":"io.tweetninja.cli.Tasks"},{"l":"io.tweetninja.cli.Types"},{"l":"io.tweetninja.cli.Util"}]
